448da91e30a086b78eef530f7715d1e7b583274a
Beatrice Tan <113320828+beartisan@users.noreply.github.com>
2023-04-20 11:52:08 -0400

    added Present field

--- app/Http/Controllers/ExperiencesController.php
+++ app/Http/Controllers/ExperiencesController.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Experience;
 
 use Illuminate\Http\Request;
+use App\Models\Experience;
 
 class ExperiencesController extends Controller
 {
-    //list
     public function list()
     {
         return view('experiences.list', [
@@ -28,7 +27,7 @@ class ExperiencesController extends Controller
             'company_name' => 'required',
             'location' => 'required',
             'start_date' => 'required',
-            'end_date' => 'required',
+            'end_date' => 'nullable|in:present',
             'description' => 'required',
             'url' => 'required',
         ]);
@@ -47,6 +46,7 @@ class ExperiencesController extends Controller
             ->with('message', 'Experience has been added!');
     }
 
+
     //EDIT
     public function editForm(Experience $experience)
     {
@@ -55,32 +55,69 @@ class ExperiencesController extends Controller
         ]);
     }
 
-    public function edit(Experience $experience)
+    // public function edit(Experience $experience)
+    // {
+
+    //     $attributes = request()->validate([
+    //         'job_title' => 'required',
+    //         'company_name' => 'required',
+    //         'location' => 'required',
+    //         'start_date' => 'required',
+    //         'end_date' => 'required',
+    //         'description' => 'required',
+    //         'url' => 'required',
+    //     ]);
+
+    //     $experience->job_title = $attributes['job_title'];
+    //     $experience->company_name = $attributes['company_name'];
+    //     $experience->location = $attributes['location'];
+    //     $experience->start_date = $attributes['start_date'];
+    //     $experience->end_date = $attributes['end_date'];
+    //     $experience->description = $attributes['description'];
+    //     $experience->url = $attributes['url'];
+
+    //     $experience->save();
+
+    //     return redirect('/console/experiences/list')
+    //         ->with('message', 'Experience has been edited!');
+    // }
+
+    public function edit(Request $request, $id)
     {
+        $experience = Experience::findOrFail($id);
 
-        $attributes = request()->validate([
-            'job_title' => 'required',
-            'company_name' => 'required',
-            'location' => 'required',
-            'start_date' => 'required',
-            'end_date' => 'required',
-            'description' => 'required',
-            'url' => 'required',
-        ]);
+        if ($request->method() == 'POST') {
+            $request->validate([
+                'job_title' => 'required',
+                'company_name' => 'required',
+                'location' => 'required',
+                'start_date' => 'required|date',
+                'description' => 'required',
+                'url' => 'nullable|url',
+            ]);
 
-        $experience->job_title = $attributes['job_title'];
-        $experience->company_name = $attributes['company_name'];
-        $experience->location = $attributes['location'];
-        $experience->start_date = $attributes['start_date'];
-        $experience->end_date = $attributes['end_date'];
-        $experience->description = $attributes['description'];
-        $experience->url = $attributes['url'];
+            $experience->job_title = $request->job_title;
+            $experience->company_name = $request->company_name;
+            $experience->location = $request->location;
+            $experience->start_date = $request->start_date;
 
-        $experience->save();
+            if ($request->has('present')) {
+                $experience->end_date = null;
+            } else {
+                $experience->end_date = $request->end_date;
+            }
 
-        return redirect('/console/experiences/list')
-            ->with('message', 'Experience has been edited!');
+            $experience->description = $request->description;
+            $experience->url = $request->url;
+            $experience->save();
+
+            return redirect('/console/experiences/list');
+        }
+
+        return view('experiences.edit', compact('experience'));
     }
+
+
 
     //DELETE
     public function delete(Experience $experience)
@@ -91,4 +128,8 @@ class ExperiencesController extends Controller
             ->with('message', 'Experience has been deleted!');
 
     }
+
+
+
+
 }
